c9cb90c81c30f16ff342cb917b6e9204e3ed571d
Paulo Vitor de Oliveira Santos <paulo@Paulos-MacBook-Pro.local>
2023-07-19 21:30:33 -0300

    adding defer

--- main.go
+++ main.go
@@ -71,6 +71,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close() // espera tudo rodar e depois executa o close
 	orderRepository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPrice(orderRepository)
 	input := usecase.OrderInput{
