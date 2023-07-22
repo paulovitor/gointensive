ff76fb8fe793cbec6f56c59458252555b83bdad1
Paulo Vitor de Oliveira Santos <paulo@Paulos-MacBook-Pro.local>
2023-07-22 17:31:18 -0300

    aula 2

--- main.go
+++ main.go
@@ -1,87 +1,50 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-
-	_ "github.com/mattn/go-sqlite3" // foi preciso add _ porque não tem uma referência direta ao import
-	"github.com/paulovitor/gointensive/internal/entity"
-	"github.com/paulovitor/gointensive/internal/infra/database"
-	"github.com/paulovitor/gointensive/internal/usecase"
+	"time"
 )
 
-type Car struct { // similar a uma classe
-	Model string
-	Color string
+func processando() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		time.Sleep(time.Second)
+	}
 }
 
-var x string // declara somente
-
-// método: função referente a struct
-func (c Car) Start() {
-	println(c.Model + " has been started")
-}
-
-// função comum
-func soma(x, y int) int { // quando os 2 parâmetros são iguais não precisa repetir o tipo
-	return x + y
-}
-
-// * ponteiro - aponta para o endereço onde determinado valor está na memória
-func (c *Car) ChangeColor(color string) {
-	c.Color = color // duplicando o valor de c.Color na memória - cópia do color original
-	println("New color: " + c.Color)
-}
-
+// T1
 func main() {
-	order, err := entity.NewOrder("1", 10, -1)
-	if err != nil {
-		println(err.Error())
-		// println(order.ID) // nil pointer dereference
-	} else {
-		println(order.ID)
+	// go processando() // T2
+	// go processando() // T3
+	// processando()    // T1
+
+	canal := make(chan int) // cria um canal de comunicação
+
+	go func() {
+		// canal <- 1 // T2 - enche o canal <- valor
+		for i := 0; i < 10; i++ {
+			canal <- i
+			fmt.Println("Jogou no canal", i)
+		}
+	}()
+
+	// fmt.Println(<-canal) // esvazia o <-canal
+
+	// time.Sleep(time.Second * 2)
+
+	// for x := range canal {
+	// 	fmt.Println(x)
+	// 	fmt.Println("Recebeu do canal", x)
+	// 	time.Sleep(time.Second)
+	// }
+
+	go worker(canal, 1)
+	worker(canal, 2)
+}
+
+func worker(canal chan int, workerId int) { // recebe valores em um canal
+	for { // loop infinito
+		fmt.Println("Recebeu do canal", <-canal, "no worker", workerId)
+		time.Sleep(time.Second)
 	}
-
-	a := 10
-	// b := a // copiou o valor de a, mas criou o seu próprio espaço na memória
-	b := &a // b está apontando para o valor na memória de a
-	*b = 20 // esta pedindo que seja alterado o valor na memória quando usa-se o ponteiro *
-
-	println(&a) // & é o endereço da variável a na memória
-	println(b)
-	println(a)
-
-	car := Car{ // := está declarando e atribuindo a variável
-		Model: "Ferrari",
-		Color: "Red",
-	}
-
-	car.Model = "Fiat" // altera o valor da variável
-
-	println(car.Model)
-
-	println("Hello, World!")
-
-	car.Start()
-
-	car.ChangeColor("Blue")
-	println(car.Color)
-
-	db, err := sql.Open("sqlite3", "db.sqlite3")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close() // espera tudo rodar e depois executa o close
-	orderRepository := database.NewOrderRepository(db)
-	uc := usecase.NewCalculateFinalPrice(orderRepository)
-	input := usecase.OrderInput{
-		ID:    "1234",
-		Price: 10.0,
-		Tax:   1.0,
-	}
-	output, err := uc.Execute(input)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(output)
 }
